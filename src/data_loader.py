c6e43691f13dd9480595adcb89371f3075b6e993
pwatana <pwatanabe22@gmail.com>
2025-06-25 10:25:35 -0400

    src removed

--- src/data_loader.py
+++ src/data_loader.py
@@ -10,7 +10,7 @@ from sklearn.utils import resample # Import for resampling
 from PIL import Image, UnidentifiedImageError # Import for image validation
 
 
-from src.config import RAW_DATA_DIR, PROCESSED_DATA_DIR, IMG_HEIGHT, IMG_WIDTH, BATCH_SIZE, RANDOM_SEED, VALIDATION_SPLIT, TEST_SPLIT, IMG_CHANNELS, RA_SCORE_THRESHOLD
+from config import RAW_DATA_DIR, PROCESSED_DATA_DIR, IMG_HEIGHT, IMG_WIDTH, BATCH_SIZE, RANDOM_SEED, VALIDATION_SPLIT, TEST_SPLIT, IMG_CHANNELS, RA_SCORE_THRESHOLD
 
 
 def prepare_data_directories():
